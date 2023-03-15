36d18e61ad28cf6ad6fb098fe6a76a38c89c3fa5
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-15 14:12:18 -0300

    refactor: Reajustado caso de uso createRecordrole com tratativas de error do zod.

--- src/application/usecases/createRecordRole/CreateRecordRoleController.ts
+++ src/application/usecases/createRecordRole/CreateRecordRoleController.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 
 import { CreateRecordRoleUseCase } from "./CreateRecordRoleUseCase";
-import { IRecordRolesRequestDTO } from "./CreateRecordRoleDTO";
+import { CreateRecordRoleRequestSchema, IRecordRolesRequestDTO } from "./CreateRecordRoleDTO";
+import { ZodError } from "zod";
 
 
 export class CreateRecordRoleController {
@@ -9,15 +10,18 @@ export class CreateRecordRoleController {
     }
 
     async handle(request: Request, response: Response) {
-        const { userId, rolesIds, adjusterId }: IRecordRolesRequestDTO = request.body;
         try {
+            const { userId, rolesIds, adjusterId } = CreateRecordRoleRequestSchema.parse(request.body);
             const errorExist = await this.createRecordRoleUsecase.execute({ userId, rolesIds, adjusterId });
             if (errorExist instanceof Error) {
                 return response.status(400).json({ error: errorExist.message });
             }
-            return response.status(200).json({});
-        } catch (error) {
-            return response.status(500).json({ errorMenssage: "Error Unexpected", error });
+            return response.status(200).json();
+        } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors);
+            }
+            return response.status(500).json({ errorMenssage: "Error Unexpected", typeErrors: errors });
         }
 
     }
--- src/application/usecases/createRecordRole/CreateRecordRoleDTO.ts
+++ src/application/usecases/createRecordRole/CreateRecordRoleDTO.ts
@@ -1,3 +1,4 @@
+import { array, string, z } from "zod"
 
 
 export interface IRecordRolesRequestDTO {
@@ -5,3 +6,11 @@ export interface IRecordRolesRequestDTO {
     adjusterId: string,
     rolesIds: string[]
 }
+
+export const CreateRecordRoleRequestSchema = z.object({
+    userId: z.string(),
+    adjusterId: z.string(),
+    rolesIds: z.array(z.string())
+        .refine(array => array.length >= 0, "Not information rolesIds in request")
+})
+

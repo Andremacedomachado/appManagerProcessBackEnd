c2e93447088630786953e534235e507cb6e7710b
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-15 13:40:18 -0300

    refactor: Reajustado caso de uso  changeWorkerStatus com tratativa errors com zod.

--- src/application/usecases/changeWorkerStatus/ChangeWorkerStatusController.ts
+++ src/application/usecases/changeWorkerStatus/ChangeWorkerStatusController.ts
@@ -1,14 +1,15 @@
 import { Request, Response } from "express";
 import { ChangeWorkerStatusUseCase } from "./ChangeWorkerStatusUseCase";
-import { IChangeWorkerStatusRequestDTO } from "./ChangeWorkerStatusDTO";
+import { ChangeStatusRequestSchema, IChangeWorkerStatusRequestDTO } from "./ChangeWorkerStatusDTO";
+import { UserInfoResponseSchema, UserInfoResponseType } from "../getAllUsers/GetAllUsersDTO";
+import { ZodError } from "zod";
 
 export class ChangeWorkerStatusController {
     constructor(private changeWorkerStatusUseCase: ChangeWorkerStatusUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { userId, status } = request.body
-
         try {
+            const { userId, status } = ChangeStatusRequestSchema.parse(request.body);
             const userUpdatedOrError = await this.changeWorkerStatusUseCase.execute({
                 id: userId,
                 status,
@@ -16,9 +17,22 @@ export class ChangeWorkerStatusController {
             if (userUpdatedOrError instanceof Error) {
                 return response.status(500).json({ error: userUpdatedOrError.message });
             }
-            return response.status(200).json(userUpdatedOrError);
-        } catch (error) {
-            return response.status(500).json({ error: 'Unexpected Error' });
+            const { name, email, organization_sector_id, status: statusResponse, created_at, updated_at } = userUpdatedOrError.props
+            const reponseInFormat = UserInfoResponseSchema.parse({
+                id: userUpdatedOrError.id,
+                name,
+                email,
+                created_at,
+                updated_at,
+                organization_sector_id,
+                status: statusResponse
+            } as UserInfoResponseType)
+            return response.status(200).json(reponseInFormat);
+        } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues)
+            }
+            return response.status(500).json({ error: 'Unexpected Error', typeErrors: errors });
         }
     }
 }
--- src/application/usecases/changeWorkerStatus/ChangeWorkerStatusDTO.ts
+++ src/application/usecases/changeWorkerStatus/ChangeWorkerStatusDTO.ts
@@ -1,5 +1,12 @@
+import { z } from "zod";
 import { IUserUpdateProps } from "../../repositories/IUserRepository";
+import { UserIsActive } from "../../../domain/entities/User";
 
 export interface IChangeWorkerStatusRequestDTO extends IUserUpdateProps {
 
 }
+
+export const ChangeStatusRequestSchema = z.object({
+    userId: z.string().uuid(),
+    status: z.enum([UserIsActive.ACTIVE, UserIsActive.INACTIVE])
+});

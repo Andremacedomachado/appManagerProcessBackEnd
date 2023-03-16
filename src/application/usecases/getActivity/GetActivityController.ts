6ec36c554cb0072ad7af969766977f0f42ee9047
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-16 14:40:54 -0300

    refactor: Reajustado caso de uso getActivity com tratativas de error do zod.

--- src/application/usecases/getActivity/GetActivityController.ts
+++ src/application/usecases/getActivity/GetActivityController.ts
@@ -1,21 +1,38 @@
 import { Request, Response } from "express";
 import { GetActivityUseCase } from "./GetActivityUseCase";
-import { IGetActivityRequestDTO } from "./GetActivityDTO";
+import { ActivityReponseType, GetActivityRequestSchema, GetActivityResponseSchema, IGetActivityRequestDTO } from "./GetActivityDTO";
+import { ZodError } from "zod";
 
 export class GetActivityController {
     constructor(private getActivityUseCase: GetActivityUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { title } = request.body
         try {
+            const { title } = GetActivityRequestSchema.parse(request.body);
             const activityOrError = await this.getActivityUseCase.execute({ title } as IGetActivityRequestDTO);
 
             if (activityOrError instanceof Error) {
                 return response.status(400).json({ error: activityOrError.message });
             }
-            return response.status(200).json(activityOrError);
-        } catch (error) {
-            return response.status(500).json({ error: 'Unexpected Error' });
+            const { title: titleFound, responsible_id, created_at, description, due_date, progress_status, start_date, type_node, updated_at } = activityOrError.props
+            const responseInFormat = GetActivityResponseSchema.parse({
+                id: activityOrError.id,
+                title: titleFound,
+                description,
+                responsible_id,
+                start_date,
+                due_date,
+                progress_status,
+                type_node,
+                created_at,
+                updated_at
+            } as ActivityReponseType)
+            return response.status(200).json(responseInFormat);
+        } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues);
+            }
+            return response.status(500).json({ error: 'Unexpected Error', typeErrors: errors });
         }
     }
 }
--- src/application/usecases/getActivity/GetActivityDTO.ts
+++ src/application/usecases/getActivity/GetActivityDTO.ts
@@ -1,4 +1,27 @@
+import { z } from "zod"
+import { STATUSACTIVITY, TYPENODE } from "../../../domain/entities/Activity"
 
 export interface IGetActivityRequestDTO {
     title: string
 }
+
+export const GetActivityRequestSchema = z.object({
+    title: z.string().min(3)
+})
+
+export const GetActivityResponseSchema = z.object({
+    id: z.string().uuid(),
+    title: z.string().min(3),
+    description: z.string(),
+    responsible_id: z.string().uuid(),
+    start_date: z.date().optional()
+        .transform(date => !date ? null : date),
+    due_date: z.date().optional()
+        .transform(date => !date ? null : date),
+    progress_status: z.enum([STATUSACTIVITY.DO_TO, STATUSACTIVITY.CLOSED]),
+    type_node: z.enum([TYPENODE.INITIAL, TYPENODE.FINALLY]).nullable(),
+    created_at: z.date(),
+    updated_at: z.date()
+})
+
+export type ActivityReponseType = z.input<typeof GetActivityResponseSchema>

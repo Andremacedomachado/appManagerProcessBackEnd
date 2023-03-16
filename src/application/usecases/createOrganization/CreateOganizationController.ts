b9da893911a7f6d757c12fc15c7ce188f74e306e
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-16 08:51:57 -0300

    refactor: Reajustado caso de uso createOrganization com tratativas de error do zod.

--- src/application/usecases/createOrganization/CreateOganizationController.ts
+++ src/application/usecases/createOrganization/CreateOganizationController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { CreateOrganizationUsecase } from "./CreateOrganizationUseCase";
-import { CreateOrganizationRequestDTO } from "./CreateOrganizationDTO";
+import { CreateOrganizationRequestDTO, CreateOrganizationRequestSchema, CreateOrganizationResponseSchema } from "./CreateOrganizationDTO";
+import { ZodError } from "zod";
 
 
 export class CreateOrganizationController {
@@ -8,15 +9,19 @@ export class CreateOrganizationController {
     constructor(private createOrganizationUseCase: CreateOrganizationUsecase) { };
 
     async handle(request: Request, response: Response) {
-        const { name, employeesAllocated, created_at, updated_at }: CreateOrganizationRequestDTO = request.body;
 
         try {
+            const { name, employeesAllocated, created_at, updated_at } = CreateOrganizationRequestSchema.parse(request.body);
             const organizationIdOrError = await this.createOrganizationUseCase.excute({ name, employeesAllocated, created_at, updated_at });
             if (organizationIdOrError instanceof Error) {
                 return response.status(500).json({ error: organizationIdOrError.message });
             }
-            return response.status(200).json(organizationIdOrError);
-        } catch (error) {
+            const responseInFormat = CreateOrganizationResponseSchema.parse(organizationIdOrError)
+            return response.status(200).json(responseInFormat);
+        } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues);
+            }
             return response.status(500).json({ error: 'Unexpected Error' });
         }
     };
--- src/application/usecases/createOrganization/CreateOrganizationDTO.ts
+++ src/application/usecases/createOrganization/CreateOrganizationDTO.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 
 
 export interface CreateOrganizationRequestDTO {
@@ -6,3 +7,16 @@ export interface CreateOrganizationRequestDTO {
     created_at?: Date,
     updated_at?: Date,
 }
+
+export const CreateOrganizationRequestSchema = z.object({
+    name: z.string(),
+    employeesAllocated: z.number().nonnegative(),
+    created_at: z.date().optional()
+        .transform(date => date ? date : new Date()),
+    updated_at: z.date().optional()
+        .transform(date => date ? date : new Date()),
+})
+
+export const CreateOrganizationResponseSchema = z.object({
+    id: z.string().uuid(),
+})

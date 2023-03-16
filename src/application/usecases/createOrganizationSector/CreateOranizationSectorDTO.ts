bbad7778f0e3865044e41be717d1dda2ee45f7fa
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-16 09:14:35 -0300

    refactor: Reajustado caso de uso createOrganizationSector com tratativas de error do zod.

--- src/application/usecases/createOrganizationSector/CreateOranizationSectorDTO.ts
+++ src/application/usecases/createOrganizationSector/CreateOranizationSectorDTO.ts
@@ -1,3 +1,4 @@
+import { z } from "zod"
 
 export interface IOrganizationSectorRequestDTO {
     name: string
@@ -6,3 +7,17 @@ export interface IOrganizationSectorRequestDTO {
     employeesAllocated: number
     organization_id: string
 }
+
+export const CreateOrganizationSectorRequestSchema = z.object({
+    name: z.string(),
+    created_at: z.date().optional()
+        .transform(date => date ? date : new Date()),
+    updated_at: z.date().optional()
+        .transform(date => date ? date : new Date()),
+    employeesAllocated: z.number().nonnegative(),
+    organization_id: z.string().uuid()
+})
+
+export const CreateOrganizationSectorResponseSchema = z.object({
+    id: z.string().uuid()
+})
--- src/application/usecases/createOrganizationSector/CreateOrganizationSectorController.ts
+++ src/application/usecases/createOrganizationSector/CreateOrganizationSectorController.ts
@@ -1,20 +1,24 @@
 import { Request, Response } from "express";
 import { CreateOrganizationSectorUseCase } from "./CreateOrganizationSectorUseCase";
-import { IOrganizationSectorRequestDTO } from "./CreateOranizationSectorDTO";
-
+import { CreateOrganizationSectorRequestSchema, CreateOrganizationSectorResponseSchema, IOrganizationSectorRequestDTO } from "./CreateOranizationSectorDTO";
+import { ZodError } from "zod";
 export class CreateOrganizationSectorController {
     constructor(private createOrganizationSectorUseCase: CreateOrganizationSectorUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { name, created_at, updated_at, employeesAllocated, organization_id } = request.body;
         try {
+            const { name, created_at, updated_at, employeesAllocated, organization_id } = CreateOrganizationSectorRequestSchema.parse(request.body);
             const sectorIdOrError = await this.createOrganizationSectorUseCase.execute({ name, created_at, updated_at, employeesAllocated, organization_id } as IOrganizationSectorRequestDTO)
             if (sectorIdOrError instanceof Error) {
                 return response.status(500).json({ error: sectorIdOrError.message })
             }
-            return response.status(200).json(sectorIdOrError);
-        } catch (error) {
-            return response.status(500).json({ error: 'Unexpected Error' })
+            const responseInFormat = CreateOrganizationSectorResponseSchema.parse(sectorIdOrError)
+            return response.status(200).json(responseInFormat);
+        } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues);
+            }
+            return response.status(500).json({ error: 'Unexpected Error', typeErros: errors })
         }
     }
 }

72d2af510b15eb09d6a130d8667b45de0afb645d
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-17 13:32:23 -0300

    refactor: Reajustado caso de uso getAnnexActivityInfo com tratativas de error do zod.

--- src/application/usecases/getAnnexActivityInfo/GetAnnexActivityInfoController.ts
+++ src/application/usecases/getAnnexActivityInfo/GetAnnexActivityInfoController.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from "express";
 import { GetAnnexActivityInfoUseCase } from "./GetAnnexActivityInfoUseCase";
-import { IGetAnnexActivityInfoRequestDTO } from "./GetAnnexActivityInfoDTO";
+import { GetAnnexActivityInfoRequestSchema, GetAnnexActivityInfoResponseSchema, IGetAnnexActivityInfoRequestDTO } from "./GetAnnexActivityInfoDTO";
+import { ZodError } from "zod";
 
 export class GetAnnexActivityInfoController {
     constructor(private getAnnexActivityInfoUseCase: GetAnnexActivityInfoUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { original_name, publication_date } = request.body
         try {
+            const { original_name, publication_date } = GetAnnexActivityInfoRequestSchema.parse(request.body)
             const annexActivityInfoOrError = await this.getAnnexActivityInfoUseCase.execute({ original_name, publication_date } as IGetAnnexActivityInfoRequestDTO);
             if (annexActivityInfoOrError instanceof Error) {
                 return response.status(400).json({ error: annexActivityInfoOrError.message });
             }
-            return response.status(200).json(annexActivityInfoOrError);
+            const responseInFormat = GetAnnexActivityInfoResponseSchema.parse(annexActivityInfoOrError)
+            return response.status(200).json(responseInFormat);
         } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(400).json(errors.issues);
+            }
             return response.status(400).json({ error: 'Unexpcted Error', typeErrors: errors });
         }
     }
--- src/application/usecases/getAnnexActivityInfo/GetAnnexActivityInfoDTO.ts
+++ src/application/usecases/getAnnexActivityInfo/GetAnnexActivityInfoDTO.ts
@@ -1,4 +1,20 @@
+import { z } from "zod"
 import { IAnnexActivityUnique } from "../../repositories/IAnnexActivityRepository"
 
 export interface IGetAnnexActivityInfoRequestDTO extends IAnnexActivityUnique {
 }
+
+export const GetAnnexActivityInfoRequestSchema = z.object({
+    original_name: z.string().min(3),
+    publication_date: z.string().datetime({ precision: 3 })
+        .transform(dateString => new Date(dateString))
+})
+
+export const GetAnnexActivityInfoResponseSchema = z.object({
+    original_name: z.string(),
+    file_name: z.string(),
+    user_id: z.string().uuid(),
+    activity_id: z.string().uuid(),
+    url: z.string(),
+    publication_date: z.date()
+})

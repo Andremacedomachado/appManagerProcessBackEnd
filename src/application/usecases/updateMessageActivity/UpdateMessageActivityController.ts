ca391e8045dede1d20c0b6b0d245b318fda184c2
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-17 10:03:26 -0300

    refactor: Reajustado caso de uso updateMessageActivity com tratativas de error do zod.

--- src/application/usecases/updateMessageActivity/UpdateMessageActivityController.ts
+++ src/application/usecases/updateMessageActivity/UpdateMessageActivityController.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from "express";
 import { UpdateMessageActivityUseCase } from "./UpdateMessageActivityUseCase";
-import { IUpdateMessageActivityRequestDTO } from "./UpdateMessageActivityDTO";
+import { IUpdateMessageActivityRequestDTO, UpdateMessageActivityRequestSchema, UpdateMessageActivityResponseSchema } from "./UpdateMessageActivityDTO";
+import { ZodError } from "zod";
 
 export class UpdateMessageActivityController {
     constructor(private updateMessageActivityUseCase: UpdateMessageActivityUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { content, type_message, updated_at, activity_id, publication_date, user_id } = request.body;
         try {
+            const { content, type_message, updated_at, activity_id, publication_date, user_id } = UpdateMessageActivityRequestSchema.parse(request.body);
             const messageUpdatedOrError = await this.updateMessageActivityUseCase.execute({ content, type_message, updated_at, activity_id, publication_date, user_id } as IUpdateMessageActivityRequestDTO);
             if (messageUpdatedOrError instanceof Error) {
                 return response.status(400).json({ error: messageUpdatedOrError.message });
             }
-            return response.status(200).json(messageUpdatedOrError);
+            const responseInFormat = UpdateMessageActivityResponseSchema.parse(messageUpdatedOrError);
+            return response.status(200).json(responseInFormat);
         } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues);
+            }
             return response.status(500).json({ error: 'Unexpected Error', typeErrors: errors });
         }
     }
--- src/application/usecases/updateMessageActivity/UpdateMessageActivityDTO.ts
+++ src/application/usecases/updateMessageActivity/UpdateMessageActivityDTO.ts
@@ -1,4 +1,28 @@
+import { z } from "zod";
 import { IMessageActivityUpdateProps } from "../../repositories/IMessageActivityRepository";
+import { TYPEMESSAGE } from "../../../domain/entities/MessageActivity";
 
 export interface IUpdateMessageActivityRequestDTO extends IMessageActivityUpdateProps {
 }
+
+export const UpdateMessageActivityRequestSchema = z.object({
+    content: z.string().optional(),
+    publication_date: z.string().datetime({ precision: 3 })
+        .transform(dateString => new Date(dateString)),
+    type_message: z.enum([TYPEMESSAGE.SYSTEM, TYPEMESSAGE.USER]).optional(),
+    user_id: z.string().uuid(),
+    activity_id: z.string().uuid(),
+    updated_at: z.string().optional()
+        .transform(dateString => dateString ? new Date(dateString) : new Date())
+})
+
+export const UpdateMessageActivityResponseSchema = z.object({
+    content: z.string(),
+    type_message: z.enum([TYPEMESSAGE.SYSTEM, TYPEMESSAGE.USER]),
+    publication_date: z.date(),
+    activity_id: z.string().uuid(),
+    user_id: z.string().uuid(),
+    updated_at: z.date()
+})
+
+export type UpdateMessageActivityResponseDTO = z.input<typeof UpdateMessageActivityResponseSchema>

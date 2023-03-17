54c343993baabdcf5ea8f42d7f6bf3f52bf2e93e
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-17 13:30:02 -0300

    refactor: Reajustado caso de uso createAnnexActivity com tratativas de error do zod.

--- src/application/usecases/createAnnexActivity/CreateAnnexActivityController.ts
+++ src/application/usecases/createAnnexActivity/CreateAnnexActivityController.ts
@@ -1,32 +1,36 @@
 import { Request, Response } from "express";
 import { CreateAnnexActivityUseCase } from "./CreateAnnexActivityUseCase";
-import { ICreateAnnexActivityRequestDTO } from "./CreateAnnexActivityDTO";
-
-
+import { CreateAnnexActivityRequestDTO, CreateAnnexActivityRequestSchema, CreateAnnexActivityResponseSchema, ICreateAnnexActivityRequestDTO } from "./CreateAnnexActivityDTO";
+import { ZodError } from "zod";
 export class CreateAnnexActivityController {
     constructor(private createAnnexActivityUseCase: CreateAnnexActivityUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { activity_id, user_id } = request.body;
-        if (!request.file) {
-            return response.status(400).json({ error: 'missing file in payload!' })
-        }
-        const { originalname: original_name, filename: file_name } = request.file
 
         try {
-            const annexActivityIdOrError = await this.createAnnexActivityUseCase.execute({
-                file_name,
+            if (!request.file) {
+                return response.status(400).json({ error: 'missing file in payload!' })
+            }
+            const { originalname: original_name, filename: file_name } = request.file
+            const { activity_id, user_id, publication_date, url } = request.body;
+            const dataRequestValid = CreateAnnexActivityRequestSchema.parse({
                 activity_id,
+                user_id,
+                file_name,
                 original_name,
-                url: '',
-                user_id
-            } as ICreateAnnexActivityRequestDTO)
-
+                publication_date,
+                url
+            } as CreateAnnexActivityRequestDTO)
+            const annexActivityIdOrError = await this.createAnnexActivityUseCase.execute(dataRequestValid)
             if (annexActivityIdOrError instanceof Error) {
                 return response.status(400).json({ error: annexActivityIdOrError.message });
             }
-            return response.status(200).json(annexActivityIdOrError);
+            const responseInFormat = CreateAnnexActivityResponseSchema.parse(annexActivityIdOrError)
+            return response.status(200).json(responseInFormat);
         } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues);
+            }
             return response.status(500).json({ error: 'Unexpected Error', typeErrors: errors });
         }
     }
--- src/application/usecases/createAnnexActivity/CreateAnnexActivityDTO.ts
+++ src/application/usecases/createAnnexActivity/CreateAnnexActivityDTO.ts
@@ -1,4 +1,23 @@
+import { z } from "zod";
 import { IAnnexActivityProps } from "../../../domain/entities/AnnexActivity";
 
 export interface ICreateAnnexActivityRequestDTO extends IAnnexActivityProps {
 }
+
+export const CreateAnnexActivityRequestSchema = z.object({
+    original_name: z.string().min(3),
+    file_name: z.string(),
+    user_id: z.string().uuid(),
+    activity_id: z.string().uuid(),
+    url: z.string().optional().transform(url => url ? url : ''),
+    publication_date: z.string().datetime().optional()
+        .transform(dateString => dateString ? new Date(dateString) : new Date())
+})
+
+export const CreateAnnexActivityResponseSchema = z.object({
+    user_id: z.string().uuid(),
+    activity_id: z.string().uuid(),
+    publication_date: z.date()
+});
+
+export type CreateAnnexActivityRequestDTO = z.input<typeof CreateAnnexActivityRequestSchema>

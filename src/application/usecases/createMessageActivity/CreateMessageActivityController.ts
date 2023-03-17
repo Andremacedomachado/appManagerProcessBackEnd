345b66f9dbfa025e7311e5f456fb4fac8d6396b9
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-17 09:20:40 -0300

    refactor: Reajustado caso de uso createMessageActivity com tratativas de error do zod.

--- src/application/usecases/createMessageActivity/CreateMessageActivityController.ts
+++ src/application/usecases/createMessageActivity/CreateMessageActivityController.ts
@@ -1,20 +1,25 @@
 import { Request, Response } from "express";
 import { CreateMessageActivityUseCase } from "./CreateMessageActivityUseCase";
-import { ICreateMessageActivityRequestDTO } from "./CreateMessageActivtyDTO";
+import { CreateMessageActivityRequestSchema, CreateMessageActivityResponseSchema, ICreateMessageActivityRequestDTO } from "./CreateMessageActivtyDTO";
+import { ZodError } from "zod";
 
 export class CreateMessageActivityController {
     constructor(private createMessageActivityUseCase: CreateMessageActivityUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { content, publication_date, updated_at, typeMessage, activityId, userId } = request.body;
 
         try {
+            const { content, publication_date, updated_at, typeMessage, activityId, userId } = CreateMessageActivityRequestSchema.parse(request.body);
             const messageIdOrError = await this.createMessageActivityUseCase.execute({ content, publication_date, updated_at, typeMessage, activityId, userId } as ICreateMessageActivityRequestDTO);
             if (messageIdOrError instanceof Error) {
                 return response.status(400).json({ error: messageIdOrError.message });
             }
-            return response.status(200).json(messageIdOrError);
+            const responseInFormat = CreateMessageActivityResponseSchema.parse(messageIdOrError);
+            return response.status(200).json(responseInFormat);
         } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues);
+            }
             return response.status(500).json({ error: 'Unexpected Error', typeErrors: errors });
         }
     }
--- src/application/usecases/createMessageActivity/CreateMessageActivtyDTO.ts
+++ src/application/usecases/createMessageActivity/CreateMessageActivtyDTO.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+import { TYPEMESSAGE } from "../../../domain/entities/MessageActivity";
 
 export interface ICreateMessageActivityRequestDTO {
     content: string;
@@ -7,3 +9,20 @@ export interface ICreateMessageActivityRequestDTO {
     userId: string;
     activityId: string;
 }
+
+export const CreateMessageActivityRequestSchema = z.object({
+    content: z.string(),
+    publication_date: z.string().optional()
+        .transform(dateString => dateString ? new Date(dateString) : new Date()),
+    typeMessage: z.enum([TYPEMESSAGE.SYSTEM, TYPEMESSAGE.USER]),
+    userId: z.string().uuid(),
+    activityId: z.string().uuid(),
+    updated_at: z.string().optional()
+        .transform(dateString => dateString ? new Date(dateString) : new Date())
+})
+
+export const CreateMessageActivityResponseSchema = z.object({
+    user_id: z.string().uuid(),
+    publication_date: z.date(),
+    activity_id: z.string().uuid(),
+})

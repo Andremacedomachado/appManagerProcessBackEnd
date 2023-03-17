152e30b0ebe9ca42effd151b596627a76acef1d3
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-17 13:31:15 -0300

    refactor: Reajustado caso de uso deleteAnnexActivity com tratativas de error do zod.

--- src/application/usecases/deleteAnnexActivity/DeleteAnnexActivityController.ts
+++ src/application/usecases/deleteAnnexActivity/DeleteAnnexActivityController.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from "express";
 import { DeleteAnnexActivtyUseCase } from "./DeleteAnnexActivityUseCase";
+import { DeletAnnexActivityRequestSchema } from "./DeleteAnnexActivityDTO";
+import { ZodError } from "zod";
 
 export class DeleteAnnexActivityController {
     constructor(private deleteAnnexActivityUseCase: DeleteAnnexActivtyUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { activity_id, publication_date, user_id } = request.body;
         try {
+            const { activity_id, publication_date, user_id } = DeletAnnexActivityRequestSchema.parse(request.body);
             const resultDeleteOrError = await this.deleteAnnexActivityUseCase.execute({ activity_id, publication_date, user_id });
             if (resultDeleteOrError instanceof Error) {
                 return response.status(400).json({ error: resultDeleteOrError.message });
             }
             return response.status(200).json({ message: 'operation successful' });
         } catch (errors) {
-            return response.status(400).json({ error: 'Unexpected Error', typeErrors: errors });
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues)
+            }
+            return response.status(500).json({ error: 'Unexpected Error', typeErrors: errors });
         }
     }
 }
--- src/application/usecases/deleteAnnexActivity/DeleteAnnexActivityDTO.ts
+++ src/application/usecases/deleteAnnexActivity/DeleteAnnexActivityDTO.ts
@@ -1,4 +1,12 @@
+import { z } from "zod";
 import { IAnnexActivityIdProps } from "../../repositories/IAnnexActivityRepository";
 
 export interface IDeleteAnnexActivityRequestDTO extends IAnnexActivityIdProps {
 }
+
+export const DeletAnnexActivityRequestSchema = z.object({
+    user_id: z.string().uuid(),
+    activity_id: z.string().uuid(),
+    publication_date: z.string().datetime({ precision: 3 })
+        .transform(dataString => new Date(dataString))
+});

5534f58d51aa08068f896ecb6221df047d024fab
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-17 13:32:56 -0300

    refactor: Reajustado caso de uso getAnnexFile com tratativas de error do zod.

--- src/application/usecases/getAnnexFile/GetAnnexFileController.ts
+++ src/application/usecases/getAnnexFile/GetAnnexFileController.ts
@@ -1,21 +1,25 @@
 import { Request, Response } from "express";
 import { GetAnnexFileUseCase } from "./GetAnnexFileUseCase";
-import { IGetAnnexFileRequestDTO } from "./GetAnnexFileDTO";
-import fs from "fs";
+import { GetAnnexFileDataRequestSchema, GetAnnexFileDataResponseSchema, IGetAnnexFileRequestDTO } from "./GetAnnexFileDTO";
+import { ZodError } from "zod";
 
 export class GetAnnexFileController {
     constructor(private getAnnexFileUseCase: GetAnnexFileUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { original_name, publication_date } = request.body;
         try {
+            const { original_name, publication_date } = GetAnnexFileDataRequestSchema.parse(request.body);
             const annexPathFileOrError = await this.getAnnexFileUseCase.execute({ original_name, publication_date } as IGetAnnexFileRequestDTO);
 
             if (annexPathFileOrError instanceof Error) {
                 return response.status(400).json({ error: annexPathFileOrError.message });
             }
-            return response.download(annexPathFileOrError.path, annexPathFileOrError.name);
+            const { path, name } = GetAnnexFileDataResponseSchema.parse(annexPathFileOrError)
+            return response.download(path, name);
         } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues);
+            }
             return response.status(500).json({ error: 'Unexpcted Error', typeErrors: errors });
         }
     }
--- src/application/usecases/getAnnexFile/GetAnnexFileDTO.ts
+++ src/application/usecases/getAnnexFile/GetAnnexFileDTO.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { IAnnexActivityUnique } from "../../repositories/IAnnexActivityRepository";
 
 export interface IGetAnnexFileRequestDTO extends IAnnexActivityUnique { }
@@ -7,3 +8,15 @@ export interface IGetAnnexFileDataResponseDTO {
     name: string,
     extension: string,
 }
+
+export const GetAnnexFileDataRequestSchema = z.object({
+    original_name: z.string().min(3),
+    publication_date: z.string().datetime({ precision: 3 })
+        .transform(dateString => new Date(dateString))
+})
+
+export const GetAnnexFileDataResponseSchema = z.object({
+    path: z.string(),
+    name: z.string().min(3),
+    extension: z.string()
+})

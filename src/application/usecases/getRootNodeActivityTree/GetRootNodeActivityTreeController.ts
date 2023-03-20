68c59cd6934ca482819486877ab7533b0982654e
Andremacedomachado <andremacedomachado@gmail.com>
2023-03-20 10:39:26 -0300

    refactor: Reajustado caso de uso getRootNodeActivityTree com tratativas de error do zod.

--- src/application/usecases/getRootNodeActivityTree/GetRootNodeActivityTreeController.ts
+++ src/application/usecases/getRootNodeActivityTree/GetRootNodeActivityTreeController.ts
@@ -1,18 +1,27 @@
 import { Request, Response } from "express";
 import { GetRootNodeActivityTreeUseCase } from "./GetRootNodeActivityTreeUseCase";
+import { GetRootNodeActivityTreeRequestSchema, GetRootNodeActivityTreeResponseDTO, GetRootNodeActivityTreeResponseSchema } from "./GetRootNodeActivityTreeDTO";
+import { ZodError } from "zod";
 
 export class GetRootNodeActivityTreeController {
     constructor(private getRootNodeActivityTreeUseCase: GetRootNodeActivityTreeUseCase) { }
 
     async handle(request: Request, response: Response) {
-        const { activityId } = request.body;
         try {
+            const { activityId } = GetRootNodeActivityTreeRequestSchema.parse(request.body);
             const activityOrError = await this.getRootNodeActivityTreeUseCase.execute(activityId);
             if (activityOrError instanceof Error) {
                 return response.status(400).json({ error: activityOrError.message });
             }
-            return response.status(200).json(activityOrError);
+            const { title, description, start_date, due_date, responsible_id, progress_status, type_node, created_at, updated_at } = activityOrError.props
+            const responseInFormat = GetRootNodeActivityTreeResponseSchema.parse({
+                id: activityOrError.id, title, description, start_date, due_date, responsible_id, progress_status, type_node, created_at, updated_at
+            } as GetRootNodeActivityTreeResponseDTO);
+            return response.status(200).json(responseInFormat);
         } catch (errors) {
+            if (errors instanceof ZodError) {
+                return response.status(500).json(errors.issues);
+            }
             return response.status(500).json({ error: 'Unexpected Error', typeErrors: errors });
 
         }
---
+++ src/application/usecases/getRootNodeActivityTree/GetRootNodeActivityTreeDTO.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+import { STATUSACTIVITY, TYPENODE } from "../../../domain/entities/Activity";
+
+export const GetRootNodeActivityTreeRequestSchema = z.object({
+    activityId: z.string().uuid()
+})
+
+export const GetRootNodeActivityTreeResponseSchema = z.object({
+    id: z.string().uuid(),
+    title: z.string().min(3),
+    description: z.string(),
+    responsible_id: z.string().uuid(),
+    start_date: z.date().optional()
+        .transform(date => date ? date : null),
+    due_date: z.date().optional()
+        .transform(date => date ? date : null),
+    progress_status: z.enum([STATUSACTIVITY.DO_TO, STATUSACTIVITY.CLOSED]),
+    type_node: z.enum([TYPENODE.INITIAL, TYPENODE.FINALLY]).nullable(),
+    created_at: z.date(),
+    updated_at: z.date()
+})
+
+export type GetRootNodeActivityTreeResponseDTO = z.input<typeof GetRootNodeActivityTreeResponseSchema>
